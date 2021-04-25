944e2f5ae5acae6614f98708ef3dd299cb2b578a
Thiago Bignotto <bignotto@gmail.com>
2021-04-25 20:25:04 -0300

    save commit

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,9 +1,9 @@
-import { Component, createRef } from 'react';
-import { FiCheckSquare } from 'react-icons/fi';
+import { Component, createRef } from "react";
+import { FiCheckSquare } from "react-icons/fi";
 
-import { Form } from './styles';
-import Modal from '../Modal';
-import Input from '../Input';
+import { Form } from "./styles";
+import { Modal } from "../Modal";
+import Input from "../Input";
 
 class ModalAddFood extends Component {
   constructor(props) {
@@ -42,6 +42,6 @@ class ModalAddFood extends Component {
       </Modal>
     );
   }
-};
+}
 
 export default ModalAddFood;
