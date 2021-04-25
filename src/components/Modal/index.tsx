b1aed1b2ee1c3bb383b45b72c124237d328b1214
Thiago Bignotto <bignotto@gmail.com>
2021-04-25 19:20:09 -0300

    Modal done

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,52 @@
+import { ReactNode, useEffect, useState } from "react";
+import ReactModal from "react-modal";
+
+interface ModalProps {
+  isOpen: boolean;
+  children: ReactNode;
+}
+
+export function Modal({ isOpen, children }: ModalProps) {
+  const [modalStatus, setModalStatus] = useState<boolean>(false);
+
+  useEffect(() => {
+    setModalStatus(isOpen);
+  }, [isOpen]);
+
+  // function handleOpenModal() {
+  //   setModalStatus(true);
+  // }
+
+  function handleCloseModal() {
+    setModalStatus(false);
+  }
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={handleCloseModal}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          background: "#F0F0F5",
+          color: "#000000",
+          borderRadius: "8px",
+          width: "736px",
+          border: "none",
+        },
+        overlay: {
+          backgroundColor: "#121214e6",
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+}
